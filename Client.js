753e9fb0812764be7eebda51542af4f061045469
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2023-03-21 12:27:26 +0530

    Update Client.js

--- Client.js
+++ Client.js
@@ -1,36 +1,18 @@
-// Sendinblue API key
-const apiKey = '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';
+const sgMail = require('@sendgrid/mail');
 
-// Sendinblue API endpoint
-const endpoint = 'https://api.sendinblue.com/v3/smtp/email';
+sgMail.setApiKey('SG.HHtI_lt3Q0-_ZBIafkOfEg.tsKtKAvWWDzHoyvzjW9WFZHvtZvoGkuWBEl82XSIWEI');
 
-// Create a new XMLHttpRequest object
-const xhr = new XMLHttpRequest();
-
-// Set the request method and endpoint
-xhr.open('POST', endpoint, true);
-
-// Set the request headers
-xhr.setRequestHeader('Accept', 'application/json');
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.setRequestHeader('api-key', apiKey);
-
-// Set the request payload
-const payload = JSON.stringify({
-    to: [{email: 'aashgates@Outlook.com'}],
-    subject: 'Test email from Sendinblue',
-    htmlContent: '<p>This is a test email sent using Sendinblue API.</p>',
-    sender: {email: 'aashik.k@albertsons.in'}
-});
-
-// Send the request
-xhr.send(payload);
-
-// Handle the response
-xhr.onload = function() {
-    if (xhr.status >= 200 && xhr.status < 300) {
-        console.log('Email sent successfully!');
-    } else {
-        console.error('Failed to send email. Error: ' + xhr.statusText);
-    }
+const msg = {
+  to: 'aashgates@outlook.com',
+  from: 'aashik.k@albertsons.in',
+  subject: 'Test Email',
+  text: 'Hello, this is a test email from Aash Javascript',
 };
+
+sgMail.send(msg)
+  .then(() => {
+    console.log('Hey The Email sent successfully! Due to Security issues and the Account being a Free Account Please Check the Spam or Junk Folder');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
